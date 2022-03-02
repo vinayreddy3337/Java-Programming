c79ccf2f481edd8d2b3fa6e8b579b6a4158271f8
vinayreddy3337 <97912978+vinayreddy3337@users.noreply.github.com>
2022-03-02 09:14:13 -0800

    Create TreeSet.java

---
+++ Collection/Set/TreeSet.java
@@ -0,0 +1,22 @@
+package collection.Set;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+		Set<String> list= new TreeSet<>();
+		list.add("Java");
+		list.add("Python");
+		list.add("C");
+		list.add("JavaScript");
+		Iterator<String> itr=list.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+		System.out.println("Removed Element: "+list.remove("C"));
+		
+	}
+
+}
